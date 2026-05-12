de9b58253e3e2852b1a1c937f356beaf9c563b15
KIRUBANANTHAN D <dkirubananthan1@gmail.com>
2026-05-12 21:49:32 +0530

    Auto create superuser

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from . import create_superuser
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+
+class BackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backend'
+
+    def ready(self):
+        from . import create_superuser
